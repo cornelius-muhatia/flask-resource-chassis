60f64fa3712db37c84a01dfac5b5b9938b93b9de
Cornelius Muhatia <muhatiacornelius@gmail.com>
2020-09-21 16:43:24 +0300

    Done resource handlers

---
+++ tests/test_services.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+
+class TestChassisService(TestCase):
+    pass
+    # def setUp(self):
+    #     self.client = test_app.test_client()
+    #
+    # def test_creation(self):
+    #     response = self.client.post("/v1/person")
+    #     self.assertEqual(response.status_code, 400, "Creation validation test")
